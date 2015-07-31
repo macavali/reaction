0726e078efb8750c085d547e06508e5b9df38937
William Waites <wwaites@tardis.ed.ac.uk>
2015-07-31 20:22:56 +0100

    command line options for annotation, materialisation and normalisation

--- README.md
+++ README.md
@@ -29,5 +29,31 @@ Flow.Kappa> declare [complex| A(x!1), A(x~2!1,y~p), A(x~1), B(u,v,w~1) |]
 ```
  * Look at a materialised RDF version of an example model
 ```
-% ./dist/build/krdf/krdf -f examples/tcs.kappa
+% ./dist/build/krdf/krdf -f examples/tcs.kappa -a -n -m
+```
+
+### The krdf tool
+
+The krdf tool transforms annotated Kappa files into RDF. By default
+it will produce an empty graph as output. It has command line flags
+to extract the annotations from the comments in the file (see the
+`examples/tcs.kappa` file), to normalise agent patterns in rules
+according to the declarations, and to materialise the rules into
+RDF. All three of these options used together will extract the maximum
+information from the source file. Using the normalise flag only makes
+sense together with materialise since it operates on the information
+latent in the Kappa rules.
+```
+Kappa -> RDF
+
+Usage: krdf (-f|--filename FILENAME) [-a|--annotations] [-m|--materialise]
+            [-n|--normalise]
+  Transform Kappa rules to RDF
+
+Available options:
+  -h,--help                Show this help text
+  -f,--filename FILENAME   Kappa file to read
+  -a,--annotations         Extract annotations
+  -m,--materialise         Materialise Kappa statements to RDF
+  -n,--normalise           Normalise agent patterns according to declarations
 ```
--- krdf.hs
+++ krdf.hs
@@ -6,28 +6,60 @@ import Flow.Kappa
 import Flow.Kappa.Rdf
 import Options.Applicative
 import System.IO(stdout)
+import Swish.RDF(RDFGraph, emptyRDFGraph, getNamespaces, setNamespaces)
 import Swish.RDF.Formatter.Turtle(formatGraphAsText)
 
 data Config = Cfg { filename :: String
+                  , doAnnotate    :: Bool
+                  , doMaterialise :: Bool
+                  , doNormalise   :: Bool
                   }
 args :: Parser Config
 args = Cfg
        <$> strOption
-         ( short 'f'
-           <> metavar "FILENAME"
-           <> help "Kappa file to read" )
+       ( short 'f'
+         <> long "filename"
+         <> metavar "FILENAME"
+         <> help "Kappa file to read" )
+       <*> switch
+       ( short 'a'
+         <> long "annotations"
+         <> help "Extract annotations" )
+       <*> switch
+       ( short 'm'
+         <> long "materialise"
+         <> help "Materialise Kappa statements to RDF" )
+       <*> switch
+       ( short 'n'
+         <> long "normalise"
+         <> help "Normalise agent patterns according to declarations" )
 
-exec :: Config -> IO ()
-exec (Cfg { filename }) = do
-  input <- readFile filename
-  statements <- return $ normalise $ parseKappa input
-  annotationGraph <- return $ annotations statements
-  materialisedGraph <- return $ materialise statements annotationGraph
-  hPutStr stdout $ formatGraphAsText materialisedGraph
+genRdf :: Config -> [Statement] -> RDFGraph
+genRdf cfg kappa = materialised
+  where
+    normalised
+      | doNormalise cfg   = normalise kappa
+      | otherwise         = kappa
+    annotated
+      | doAnnotate cfg    = annotations kappa
+      | otherwise         = emptyGraph
+    materialised
+      | doMaterialise cfg = materialise normalised annotated
+      | otherwise         = annotated
+
+    -- a few hoops to jump through to get an empty graph
+    -- with the right namespace
+    nsmap      = getNamespaces $ annotations kappa
+    emptyGraph = setNamespaces nsmap emptyRDFGraph
+    
+readKappaWriteRDF :: Config -> IO ()
+readKappaWriteRDF cfg = do
+  input <- readFile $ filename cfg
+  hPutStr stdout $ formatGraphAsText $ genRdf cfg $ parseKappa input
 
 main :: IO ()
 main = do
-  execParser opts >>= exec
+  execParser opts >>= readKappaWriteRDF
   where
     opts = info (helper <*> args)
       ( fullDesc
